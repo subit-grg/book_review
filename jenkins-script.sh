5960c174b4f4570ede865077846dd291eec83f06
subit-grg <subit.grg@gmail.com>
2022-08-05 02:02:13 +0000

    updated deployment

--- jenkins-script.sh
+++ jenkins-script.sh
@@ -12,5 +12,19 @@ source venv/bin/activate
 pip3 install -r requirements.txt
 python3 -m pytest --cov=application --cov-report=html
 
+ssh jenkins@prod-server << EOF
+if [ -d book-review ]; then
+   cd book-review && git pull origin master
+else
+   git clone https://github.com/subit-grg/book_review.git book-review
+   cd book-review
+fi
+
+sudo apt install python3 python3-pip python3-venv -y
+
+python3 -m venv venv
+source venv/bin/activate
+pip3 install -r requirements.txt
 python3 create.py
-python3 app.py
+python3 -m gunicorn -D --bind 0.0.0.0:5000 --workers 4 app:app
+EOF
