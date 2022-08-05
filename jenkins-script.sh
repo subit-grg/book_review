2338a3f1a71a52778b4d42d3809acfc3cbebc578
subit-grg <subit.grg@gmail.com>
2022-08-05 00:54:33 +0000

    add jenkins script

--- jenkins-script.sh
+++ jenkins-script.sh
@@ -2,7 +2,7 @@
 
 sudo apt install python3 python3-pip python3-venv chromium-browser wget unzip -y
 
-sudo apt install wget unzip -y
+
 version=$(curl -s https://chromedriver.storage.googleapis.com/LATEST_RELEASE_$(chromium-browser --version | grep -oP 'Chromium \K\d+'))
 wget https://chromedriver.storage.googleapis.com/${version}/chromedriver_linux64.zip
 sudo unzip chromedriver_linux64.zip -d /usr/bin
